52e59528e3f9cd29d1e661a301ac32dd7a7b5a3f
shruti <shruti.javali@gmail.com>
2024-01-26 22:59:24 +0530

    deployment added

--- hello_world.js
+++ hello_world.js
@@ -1,6 +1,7 @@
 const http = require('http');
 http.createServer((req,res)=>{
     res.write("<html><h3>Hello World<h3><html>");
+    res.write("<html><br /><h3>Deployment></h3></html>");
     res.end();
 }).listen(7000);
 console.log("Server running on port 7000");
